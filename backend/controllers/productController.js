22b11ddfec9bc1bc436e2d608e39ce8df1c56d36
XdebronneX <martelevans42@gmail.com>
2024-07-29 05:23:59 -0700

    Product cruds

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@ const ProductModel = require("../models/product");
 const cloudinary = require("cloudinary");
 const ErrorHandler = require("../utils/errorHandler");
 
+//** Admin control */
 exports.newProduct = async (req, res, next) => {
   try {
     // Check if the project name already exists in the database
@@ -49,6 +50,71 @@ exports.newProduct = async (req, res, next) => {
   }
 };
 
+exports.updateProduct = async (req, res, next) => {
+  try {
+    let product = await ProductModel.findById(req.params.id);
+
+    if (!product) {
+      return next(new ErrorHandler("Product not found", 404));
+    }
+
+    let images = [];
+
+    if (typeof req.body.images === "string") {
+      images.push(req.body.images);
+    } else {
+      images = req.body.images;
+    }
+
+    if (images !== undefined) {
+      // Deleting images associated with the product
+      for (let i = 0; i < product.images.length; i++) {
+        await cloudinary.v2.uploader.destroy(product.images[i].public_id);
+      }
+
+      let imagesLinks = [];
+
+      for (let i = 0; i < images.length; i++) {
+        const result = await cloudinary.v2.uploader.upload(images[i], {
+          folder: "products",
+        });
+
+        imagesLinks.push({
+          public_id: result.public_id,
+          url: result.secure_url,
+        });
+      }
+
+      req.body.images = imagesLinks;
+    }
+
+    product = await ProductModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    next(new ErrorHandler(error.message, 500));
+  }
+};
+
+exports.deleteProduct = async (req, res, next) => {
+  const product = await ProductModel.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  await product.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Product deleted",
+  });
+};
+
 exports.getProducts = async (req, res, next) => {
   try {
     const productsCount = await ProductModel.countDocuments();
@@ -81,69 +147,4 @@ exports.getSingleProduct = async (req, res, next) => {
   } catch (error) {
     return next(new ErrorHandler(error.message, 500));
   }
-};
-
-exports.updateProduct = async (req, res, next) => {
-  let product = await ProductModel.findById(req.params.id);
-
-  if (!product) {
-    return next(new ErrorHandler("Product not found", 404));
-  }
-
-  let images = [];
-
-  if (typeof req.body.images === "string") {
-    images.push(req.body.images);
-  } else {
-    images = req.body.images;
-  }
-
-  if (images !== undefined) {
-    // Deleting images associated with the product
-
-    for (let i = 0; i < product.images.length; i++) {
-      const result = await cloudinary.v2.uploader.destroy(
-        product.images[i].public_id
-      );
-    }
-
-    let imagesLinks = [];
-
-    for (let i = 0; i < images.length; i++) {
-      const result = await cloudinary.v2.uploader.upload(images[i], {
-        folder: "products",
-      });
-
-      imagesLinks.push({
-        public_id: result.public_id,
-        url: result.secure_url,
-      });
-    }
-
-    req.body.images = imagesLinks;
-  }
-
-  product = await ProductModel.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-    useFindAndModify: false,
-  });
-
-  res.status(200).json({
-    success: true,
-
-    product,
-  });
-};
-
-exports.deleteProduct = async (req, res, next) => {
-  const product = await ProductModel.findById(req.params.id);
-  if (!product) {
-    return next(new ErrorHandler("Product not found", 404));
-  }
-  await product.deleteOne();
-  res.status(200).json({
-    success: true,
-    message: "Product deleted",
-  });
 };
