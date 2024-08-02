9ff796fd78533899f0ce038d2d7ea2d77c596b1e
XdebronneX <martelevans42@gmail.com>
2024-08-01 23:12:37 -0700

    Setup connection of frontend and backend

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const errorMiddleware = require("./middlewares/errors")
 const users = require("./routes/user")
 const products = require("./routes/product")
@@ -9,6 +10,11 @@ const products = require("./routes/product")
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(cors({
+    // origin: "https://fe-kendified.vercel.app",
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 // Routes
 app.use('/api/v1', users);
